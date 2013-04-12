7953c612afe6631c39e0b74518eb941fbff668d3
j3harvey <j3harvey@gmail.com>
2013-04-12 14:14:41 +0100

    Added separate file for polymorphism and divergence functions.

--- scratch.py
+++ scratch.py
@@ -5,7 +5,7 @@ import time
 
 inputFile = "ag5am5par1era4_allGenes_StopCodonsRemoved.fasta"
 
-def fastaRecords( fastaFile ):
+def fastaRecords():
   """
   fastaFile: a file containing input sequence data
   
@@ -21,7 +21,7 @@ def fastaRecords( fastaFile ):
   # Your data may be arranged differently!
   # If so, you will have to implement your own function below or rearrange
   # your data.
-  seqs = Bio.SeqIO.parse( fastaFile, 'fasta' )
+  seqs = Bio.SeqIO.parse( inputFile, 'fasta' )
   record = []
   for seq in seqs:
     if len(record) == 2:
@@ -31,6 +31,15 @@ def fastaRecords( fastaFile ):
       record.append((seq.id, seq.seq.upper().tostring()))
   if len(record) == 2:
     yield record
+
+def printHardSites():
+  count = 0
+  for record in fastaRecords():
+    for i in range(0,len(record[0][1]),3):
+      site = [x[1][i:i+3] for x in record]
+      if any( [ sum( [b in "YKMSRW" for b in c] ) > 1 for c in site ] ):
+        count += 1
+        print count, ":  ", site
 
 def anyMultiChange( seq1, seq2 ):
   diffs = [ (x!=y) & (x!="N") & (y!="N")  for (x,y) in zip( list(seq1), list(seq2) ) ]
@@ -70,8 +79,8 @@ def threeChange( seq1, seq2 ):
       return i
 
 
-with open( scratch.inputFile, 'r' ) as fastaFile:
-  for record in scratch.fastaRecords( fastaFile ):
+with open( inputFile, 'r' ) as fastaFile:
+  for record in fastaRecords():
     if threeChange( record[0][1], record[1][1] ):
       i = threeChange( record[0][1], record[1][1] )
       print record[0][0], i, len(record[0][1]), record[0][1][i:i+3], record[1][1][i:i+3]
