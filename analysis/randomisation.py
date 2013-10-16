ad614c8bee1bc12cb4a5ec3d31e6da8086d9ad00
j3harvey <j3harvey@gmail.com>
2013-10-16 20:39:07 +0100

    Deleted those pesky files

--- analysis/randomisation.py
+++
@@ -1,7 +0,0 @@
-with open('randomisation.mkout', 'r') as f:
-    LnL = [float(l.rstrip().split('\t')[0]) for l in f.readlines()][1:]
-
-LnL0 = LnL[0]
-LnL = LnL[1:]
-print sum([x > LnL0 for x in LnL])/float(len(LnL))
-
